bba4717214b51d96813da874011a52d78aaf0cf6
AndiiSiryi <130078717+AndiiSiryi@users.noreply.github.com>
2023-08-30 21:26:43 +0300

    Завдання 3 - генератор промісів

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
   if (shouldResolve) {
-    // Fulfill
+   resolve({ position, delay });
   } else {
-    // Reject
+     reject({ position, delay });
   }
+}, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const firstDelay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+    const delay = firstDelay + (i - 1) * step;
+
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
